33d5fd7b9860acfc0fa5260dc57f66d84d5ee637
Sameer Kamble <sameerkamble1130@gmail.com>
2024-10-02 17:40:07 -0400

    added working of result

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic';
 import Ipform from "@/components/IpForm";
 import Result from "@/components/Result";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Dynamically import the OpenLayersMap to prevent SSR issues
 const OpenLayersMap = dynamic(() => import('@/components/Map'), {
@@ -14,17 +14,37 @@ const OpenLayersMap = dynamic(() => import('@/components/Map'), {
 
 export default function Home() {
 const [ipAddress, setIpAddress] = useState("");
+const [ipDetails, setIpDetails] = useState<any>(null);
+
 
   const handleSearch = (value: string) =>{
     setIpAddress(value);
   }
+
+  useEffect(() =>{
+    if(ipAddress){
+      fetch(`/api/geo?ip=${ipAddress}`)
+      .then((response) => {
+        if(!response.ok){
+          throw new Error("Network Error");
+        }
+        return response.json();
+      })
+      .then((data) =>{
+        setIpDetails(data)
+      }).catch((error) =>{
+        console.error("Error fetching the data", error);
+        setIpDetails(null);
+      })
+    }
+  }, [ipAddress])
   return (
     <div>
       <div>
         <Ipform onSearch={handleSearch}/>
       </div>
       <div>
-        <Result />
+        <Result ipAddress={ipAddress} ipDetails={ipDetails}/>
       </div>
       <OpenLayersMap />
     </div>
--- components/Result.tsx
+++ components/Result.tsx
@@ -1,40 +1,48 @@
 
+interface IpDetails{
+    query: string;
+    ip: string;
+    region: string;
+    timezone: string;
+    isp: string;
+    country: string;
 
-export default function Result() {
-    return (<div className="pl-44">
-        <div className="relative block top-20  flex justify-between max-w-6xl rounded-lg  items-center bg-white">
-            <div className="p-10 border-r border-slate-200 w-1/4 p-4">
-                <div className="text-slate-400 text-sm ">
-                    IP ADDRESS
-                </div>
-                <div className="font-bold text-4xl pt-2 text-slate-700">
-                    8.8.8.8
-                </div>
-            </div>
-            <div className="p-10 border-r border-slate-200 w-1/4 p-4">
-                <div className="text-slate-400 text-sm ">
-                Location
-                </div>
-                <div className="font-bold text-4xl pt-2 text-slate-700">
-                    London
-                </div>
-            </div>
-            <div className="p-10 border-r border-slate-200 w-1/4 p-4">
-                <div className="text-slate-400 text-sm ">
-                Timezone
-                </div>
-                <div className="font-bold text-4xl pt-2 text-slate-700">
-                    UTC
-                </div>
-            </div>
-            <div className="p-10 border-r border-slate-200 w-1/4 p-4">
-                <div className="text-slate-400 text-sm ">
-                ISP
-                </div>
-                <div className="font-bold text-4xl pt-2 text-slate-700">
-                    Google 
-                </div>
-            </div>
-        </div>
-    </div>)
 }
+    export default function Result({ipAddress, ipDetails}: {ipAddress: string, ipDetails: IpDetails}) {
+        return (<div className="pl-44">
+            <div className="relative block top-20  flex justify-between max-w-6xl rounded-lg  items-center bg-white">
+                <div className="p-10 border-r border-slate-200 w-1/4 p-4">
+                    <div className="text-slate-400 text-sm ">
+                        IP ADDRESS
+                    </div>
+                    <div className="font-bold text-4xl pt-2 text-slate-700">
+                        {ipDetails ? ipDetails.query : ipAddress}
+                    </div>
+                </div>
+                <div className="p-10 border-r border-slate-200 w-1/4 p-4">
+                    <div className="text-slate-400 text-sm ">
+                    Location
+                    </div>
+                    <div className="font-bold text-4xl pt-2 text-slate-700">
+                        {ipDetails ? `${ipDetails.region}, ${ipDetails.country}` : "N/A"}
+                    </div>
+                </div>
+                <div className="p-10 border-r border-slate-200 w-1/4 p-4">
+                    <div className="text-slate-400 text-sm ">
+                    Timezone
+                    </div>
+                    <div className="font-bold text-4xl pt-2 text-slate-700">
+                        {ipDetails ? ipDetails.timezone : "N/A"}
+                    </div>
+                </div>
+                <div className="p-10 border-r border-slate-200 w-1/4 p-4">
+                    <div className="text-slate-400 text-sm ">
+                    ISP
+                    </div>
+                    <div className="font-bold text-4xl pt-2 text-slate-700">
+                        {ipDetails ? ipDetails.isp : "N/A"} 
+                    </div>
+                </div>
+            </div>
+        </div>)
+    }
