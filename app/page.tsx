af243daa134af6755269cebe940bfcac30dc0bcd
Sameer Kamble <sameerkamble1130@gmail.com>
2024-10-02 17:05:33 -0400

    added Ipform working

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
 // app/page.tsx
-
+"use client";
 import dynamic from 'next/dynamic';
 import Ipform from "@/components/IpForm";
 import Result from "@/components/Result";
+import { useState } from 'react';
 
 // Dynamically import the OpenLayersMap to prevent SSR issues
 const OpenLayersMap = dynamic(() => import('@/components/Map'), {
   ssr: false,
 });
 
+
+
 export default function Home() {
+const [ipAddress, setIpAddress] = useState("");
+
+  const handleSearch = (value: string) =>{
+    setIpAddress(value);
+  }
   return (
     <div>
       <div>
-        <Ipform />
+        <Ipform onSearch={handleSearch}/>
       </div>
       <div>
         <Result />
