eba2b157612eee66f981ec29052edaca30cece12
Sameer Kamble <sameerkamble1130@gmail.com>
2024-10-02 13:46:59 -0400

    added route

---
+++ app/api/geo/route.ts
@@ -0,0 +1,25 @@
+
+import { NextResponse } from "next/server";
+
+export async function GET(request : Request){
+    const {searchParams} = new URL(request.url);
+    const ip = searchParams.get('ip');
+    const apiKey = process.env.GEO_API_KEY;
+
+    try{
+        const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ip}`);
+;
+        const data = await response.json();
+        return NextResponse.json(data);
+
+    }catch(error){
+        return NextResponse.json(
+            {
+                error: "Failed to fetch the data from the API"
+            },
+            {
+                status: 500
+            }
+        );
+    }
+}
